00243bd0ac781660df29e5ca6e03ade5c61a19f3
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2019-07-03 12:27:29 +0000

    Cache list of transaction IDs for blocks

---
+++ src/cache.rs
@@ -0,0 +1,34 @@
+use crate::errors::*;
+use bitcoin_hashes::sha256d::Hash as Sha256dHash;
+use lru::LruCache;
+use std::sync::Mutex;
+
+/// Cache of transaction ids in blocks
+pub struct BlockTxIDsCache {
+    map: Mutex<LruCache<Sha256dHash, Vec<Sha256dHash>>>,
+}
+
+impl BlockTxIDsCache {
+    pub fn new(capacity: usize) -> BlockTxIDsCache {
+        BlockTxIDsCache {
+            map: Mutex::new(LruCache::new(capacity)),
+        }
+    }
+
+    pub fn get_or_else<F>(
+        &self,
+        blockhash: &Sha256dHash,
+        load_txids_func: F,
+    ) -> Result<Vec<Sha256dHash>>
+    where
+        F: FnOnce() -> Result<Vec<Sha256dHash>>,
+    {
+        if let Some(txids) = self.map.lock().unwrap().get(blockhash) {
+            return Ok(txids.clone());
+        }
+
+        let txids = load_txids_func()?;
+        self.map.lock().unwrap().put(*blockhash, txids.clone());
+        Ok(txids)
+    }
+}
